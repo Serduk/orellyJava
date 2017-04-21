340a80dbd9dfbfe27c3bab7aecaa9d86b70cb344
Serdyuk <serduksergey90@gmail.com>
2017-04-21 17:49:02 +0300

    fix minor fix

--- src/workClasses/animals/LittleDog.java
+++ src/workClasses/animals/LittleDog.java
@@ -4,7 +4,7 @@ package workClasses.animals;
  * extend Dog
  * Created by serdyuk on 4/12/17.
  */
-public class LittleDog extends Dog implements HomeAnimalInterface a{
+public class LittleDog extends Dog implements HomeAnimalInterface {
     @Override
     public void roam() {
         super.roam();
