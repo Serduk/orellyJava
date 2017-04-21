16ca706fbab3b8db82c7223ffc86bef04111921a
Serdyuk <serduksergey90@gmail.com>
2017-04-21 16:59:01 +0300

    Learn Interface

---
+++ src/workClasses/animals/Duck.java
@@ -0,0 +1,7 @@
+package workClasses.animals;
+
+/**
+ * Created by serdyuk on 4/21/17.
+ */
+public class Duck {
+}
--- src/workClasses/animals/LittleDog.java
+++ src/workClasses/animals/LittleDog.java
@@ -4,10 +4,20 @@ package workClasses.animals;
  * extend Dog
  * Created by serdyuk on 4/12/17.
  */
-public class LittleDog extends Dog {
+public class LittleDog extends Dog implements HomeAnimalInterface {
     @Override
     public void roam() {
         super.roam();
         System.out.println("LittleDog roam");
+    }
+
+    @Override
+    public void setName() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
