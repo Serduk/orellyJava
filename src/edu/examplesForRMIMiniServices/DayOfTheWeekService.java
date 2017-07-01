d096b436176ac926f4cd0d3102dada356a024912
Serdyuk <serduksergey90@gmail.com>
2017-07-01 23:26:33 +0300

    add DayOfTheWeek Service exampler

--- src/edu/examplesForRMIMiniServices/DayOfTheWeekService.java
+++ src/edu/examplesForRMIMiniServices/DayOfTheWeekService.java
@@ -1,6 +1,13 @@
 package edu.examplesForRMIMiniServices;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Class for ServiceServerImpl
@@ -8,8 +15,51 @@ import javax.swing.*;
  * Created by sserdiuk on 6/30/17.
  */
 public class DayOfTheWeekService implements Service {
+    JLabel outputLabel;
+    JComboBox month;
+    JTextField day;
+    JTextField year;
+
     @Override
     public JPanel getGUIPanel() {
-        return null;
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Do it!");
+        button.addActionListener(new DoItListener());
+        outputLabel = new JLabel("date appears here");
+        DateFormatSymbols dateStuff = new DateFormatSymbols();
+        month = new JComboBox(dateStuff.getMonths());
+        day = new JTextField(8);
+        year = new JTextField(8);
+        JPanel inputPanel = new JPanel(new GridLayout(3, 2));
+
+        inputPanel.add(new JLabel("Day"));
+        inputPanel.add(day);
+        inputPanel.add(new JLabel("Month"));
+        inputPanel.add(month);
+        inputPanel.add(new JLabel("Year"));
+        inputPanel.add(year);
+
+        panel.add(inputPanel);
+        panel.add(button);
+        panel.add(outputLabel);
+
+        return panel;
+    }
+
+    private class DoItListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int monthNum = month.getSelectedIndex();
+            int dayNum = Integer.parseInt(day.getText());
+            int yearNum = Integer.parseInt(year.getText());
+            Calendar c = Calendar.getInstance();
+            c.set(Calendar.MONTH, monthNum);
+            c.set(Calendar.DAY_OF_MONTH, dayNum);
+            c.set(Calendar.YEAR, yearNum);
+            Date date = c.getTime();
+            String dayOfTheWeek = (new SimpleDateFormat("EEEE")).format(date);
+            outputLabel.setText(dayOfTheWeek);
+        }
     }
 }
